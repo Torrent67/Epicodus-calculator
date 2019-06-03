3e8c3c05f36f4e8841641fb2bd8a72acbf6983f7
Marqis Ramos and Trevor Teigen <marqis@>
2019-06-03 13:28:14 -0700

    made basic calc more fancy

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,40 @@
-var add = function(number1, number2){
-return number1 + number2;
-}
+$(document).ready(function(){
+  $("#add").click(function() {
+    var number1 = parseInt(prompt("Enter a number:"));
+    var number2 = parseInt(prompt("Enter another number:"));
+    var result= add(number1,number2);
+    alert(result);
+  });
+  $("#subtract").click(function() {
+    var number1 = parseInt(prompt("Enter a number:"));
+    var number2 = parseInt(prompt("Enter another number:"));
+    var result= subtract(number1,number2);
+    alert(result);
+  });
+  $("#multiply").click(function() {
+    var number1 = parseInt(prompt("Enter a number:"));
+    var number2 = parseInt(prompt("Enter another number:"));
+    var result= multiply(number1,number2);
+    alert(result);
+  });
+  $("#divide").click(function() {
+    var number1 = parseInt(prompt("Enter a number:"));
+    var number2 = parseInt(prompt("Enter another number:"));
+    var result= divide(number1,number2);
+    alert(result);
+  });
+});
 
-var number1=parseInt(prompt("enter a number:"));
-var number2=parseInt(prompt("enter another number:"))
-alert(add(number1, number2));
+
+var add = function(number1, number2) {
+  return number1 + number2;
+};
+var subtract = function(number1, number2) {
+  return number1 - number2;
+};
+var multiply = function(number1, number2) {
+  return number1 * number2;
+};
+var divide = function(number1, number2) {
+  return number1 / number2;
+};
